869df2b5e804554b6b9fc8cfbfed503aec1f1f1f
Suhel-Kap <suhelkapadia2@gmail.com>
2022-02-10 02:52:12 +0530

    Change SMTP port

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,31 +2,50 @@ export default function (req, res) {
     require('dotenv').config();
 
     let nodemailer = require('nodemailer');
-    const transporter = nodemailer.createTransport({
+    let testAccount = await nodemailer.createTestAccount();
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.email",
         port: 465,
-        host: "smtp.gmail.com",
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASSWORD,
         },
-        secureConnection: true,
+    });
+    // const transporter = nodemailer.createTransport({
+    //     port: 465,
+    //     host: "smtp.gmail.com",
+    //     auth: {
+    //         user: process.env.EMAIL,
+    //         pass: process.env.PASSWORD,
+    //     },
+    //     secureConnection: true,
+    // });
+    let info = await transporter.sendMail({
+        from: process.env.EMAIL, // sender address
+        to: "suhelkapadia2@gmail.com", // list of receivers
+        subject: `Message From ${req.body.name}`, // Subject line
+        text: req.body.message + " | Sent from: " + req.body.email, // plain text body
+        html: `<div>${req.body.message}</div><p>Sent from:
+        ${req.body.email}</p>`, // html body
     });
 
-    const mailData = {
-        from: process.env.EMAIL,
-        to: 'suhelkapadia2@gmail.com',
-        subject: `Message From ${req.body.name}`,
-        text: req.body.message + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.message}</div><p>Sent from:
-        ${req.body.email}</p>`
-    }
+    console.log("Message sent: %s", info.messageId);
+    // const mailData = {
+    //     from: process.env.EMAIL,
+    //     to: 'suhelkapadia2@gmail.com',
+    //     subject: `Message From ${req.body.name}`,
+    //     text: req.body.message + " | Sent from: " + req.body.email,
+    //     html: `<div>${req.body.message}</div><p>Sent from:
+    //     ${req.body.email}</p>`
+    // }
 
-    await transporter.sendMail(mailData, function (err, info) {
-        if (err)
-            console.log(err)
-        else
-            console.log(info)
-    })
+    // await transporter.sendMail(mailData, function (err, info) {
+    //     if (err)
+    //         console.log(err)
+    //     else
+    //         console.log(info)
+    // })
 
     console.log(req.body)
     res.status(200).send('Message Sent')

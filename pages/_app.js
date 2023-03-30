434774fbd0bd00ebbac2e7d9b843cff3e35c81a1
Suhel Kapadia <suhelkapadia2@gmail.com>
2023-03-30 16:40:48 +0000

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider defaultTheme="light" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider defaultTheme="light" attribute="class">
+            <Component {...pageProps} />
+            <Analytics />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
